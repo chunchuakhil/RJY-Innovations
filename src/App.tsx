3f4b5c3dcdf5346a925db8396510fb0369caaa12
Akhil CH <akhilchunchu52@gmail.com>
2022-10-29 14:13:53 +0000

    feat: Initial setup with styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import "./styles.css";
 
-import { GlobalStyles } from "../styles/Global";
-import { Header } from "../styles/Header.styled";
-import { Footer } from "../styles/Footer.styled";
+import { GlobalStyles } from "./styles/Global";
+import { Header } from "./styles/Header.styled";
+import { Footer } from "./styles/Footer.styled";
 
 export default function App() {
   return (
     <div className="App">
       <GlobalStyles />
-      <Header>Game of Thrones Quotes</Header>
-
+      <Header>Styled Header</Header>
       <Footer>
         <p>Made in India</p>
       </Footer>
